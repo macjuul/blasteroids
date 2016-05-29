a1781582579a2b93961819116bd38f6a5f0fdd31
macjuul <minecraftmacjuul@gmail.com>
2016-05-30 01:24:07 +0200

    Finished Layer System implementation

--- src/main/java/net/exodiusmc/blasteroids/Main.java
+++ src/main/java/net/exodiusmc/blasteroids/Main.java
@@ -6,11 +6,16 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import net.exodiusmc.blasteroids.layers.GameLayer;
+import net.exodiusmc.blasteroids.layers.SpaceLayer;
+import net.exodiusmc.blasteroids.manager.LayerManager;
 import net.exodiusmc.blasteroids.manager.MediaManager;
 import net.exodiusmc.blasteroids.utils.FileUtils;
 
 public class Main extends Application {
 	public Stage window;
+	public static double WIDTH;
+	public static double HEIGHT;
 
 	public static void main(String[] args) {
 		launch();
@@ -24,7 +29,9 @@ public class Main extends Application {
 		// Initalize different functions
 		FileUtils.setResourceDirectory("net/exodiusmc/blasteroids");
 		MediaManager.initialize();
+		Runtime.initalize();
 		
+		// Populize the window
 		StackPane pane = new StackPane();
 		Canvas cvs = new Canvas();
 		
@@ -33,25 +40,27 @@ public class Main extends Application {
 		Scene main = new Scene(pane);
 		window.setScene(main);
 		
+		// Set the window size
 		double height = Screen.getPrimary().getVisualBounds().getHeight() * 0.9;
 		
 		window.setWidth(height * 1.4);
 		window.setHeight(height);
 		
+		Main.WIDTH = height * 1.4;
+		Main.HEIGHT = height;
+		
+		// Add the layers
+		LayerManager.getManager().add(new SpaceLayer());
+		LayerManager.getManager().add(new GameLayer());
+		
+		// Complete window loading
 		window.getIcons().addAll(FileUtils.LoadImage("img/icon_128.png"), FileUtils.LoadImage("img/icon_32.png"), FileUtils.LoadImage("img/icon_16.png"));
-		
-		window.show();
-		
 		window.setTitle("Blasteroids " + getVersion());
-		
+		window.show();
 		Logger.getLogger().info("Blasteroids version " + getVersion() + " successfully loaded!");
-		
-		new Runtime();
-		
-		Logger.getLogger().info("Runtime loaded");
 	}
 	
-	public String getVersion() {
+	public static String getVersion() {
 		return "0.0.1 PRE-ALPHA";
 	}
 
