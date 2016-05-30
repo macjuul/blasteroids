952a73d58c9f41ebaeaa59c4763c388fc057a922
macjuul <minecraftmacjuul@gmail.com>
2016-05-30 03:29:41 +0200

    Derpy space!!

--- src/main/java/net/exodiusmc/blasteroids/layers/SpaceLayer.java
+++ src/main/java/net/exodiusmc/blasteroids/layers/SpaceLayer.java
@@ -6,12 +6,17 @@ import javafx.scene.paint.Color;
 import net.exodiusmc.blasteroids.Main;
 import net.exodiusmc.blasteroids.interfaces.Layer;
 import net.exodiusmc.blasteroids.manager.MediaManager;
+import net.exodiusmc.blasteroids.utils.GeneralUtils;
 
 public class SpaceLayer implements Layer {
 	private double spaceWidth;
     private double spaceHeight;
     private int xStart;
     private int yStart;
+    private double vel_x = 0;
+    private double pos_x = 0;
+    private double vel_y = 0;
+    private double pos_y = 0;
     private Image space;
     
     public SpaceLayer() {
@@ -27,14 +32,20 @@ public class SpaceLayer implements Layer {
 
 	@Override
 	public void update(double delta, long frame) {
-		xStart = (int) (((frame * 1.5) % spaceWidth) - spaceWidth);
-		yStart = (int) (((frame * 1.5) % spaceHeight) - spaceHeight);
+		this.vel_x += GeneralUtils.randomDoubleInRange(-1, 1.02);		
+		this.vel_y += GeneralUtils.randomDoubleInRange(-1, 1.02);
+		
+		this.pos_x += this.vel_x;
+		this.pos_y += this.vel_y;
+		
+		xStart = (int) (((pos_x * 1.5) % spaceWidth) - spaceWidth);
+		yStart = (int) (((pos_y * 1.5) % spaceHeight) - spaceHeight);
 	}
 
 	@Override
 	public void render(GraphicsContext gfx) {
 		
-        for(int x = 0; x < Main.WIDTH + spaceWidth; x += spaceWidth) {
+        for(int x = 0; x < Main.WIDTH + spaceWidth * 2; x += spaceWidth) {
             for(int y = 0; y < Main.HEIGHT * 2; y += spaceHeight) {
                 gfx.drawImage(space, x + xStart, y + yStart);
             }
