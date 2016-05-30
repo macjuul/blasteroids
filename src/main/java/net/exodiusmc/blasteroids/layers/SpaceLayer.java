083cfad6864e9ef77d24561d3618805f3ded062e
macjuul <minecraftmacjuul@gmail.com>
2016-05-30 03:06:49 +0200

    Added basic space layer

--- src/main/java/net/exodiusmc/blasteroids/layers/SpaceLayer.java
+++ src/main/java/net/exodiusmc/blasteroids/layers/SpaceLayer.java
@@ -1,26 +1,44 @@
 package net.exodiusmc.blasteroids.layers;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import net.exodiusmc.blasteroids.Main;
 import net.exodiusmc.blasteroids.interfaces.Layer;
+import net.exodiusmc.blasteroids.manager.MediaManager;
 
 public class SpaceLayer implements Layer {
+	private double spaceWidth;
+    private double spaceHeight;
+    private int xStart;
+    private int yStart;
+    private Image space;
+    
+    public SpaceLayer() {
+		this.space = MediaManager.getManager().getImage("space");
+		this.spaceHeight = space.getHeight();
+		this.spaceWidth = space.getWidth();
+	}
 
 	@Override
 	public boolean updateOnCover() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public void update(double delta, long frame) {
-		// TODO Auto-generated method stub
-		
+		xStart = (int) (((frame * 1.5) % spaceWidth) - spaceWidth);
+		yStart = (int) (((frame * 1.5) % spaceHeight) - spaceHeight);
 	}
 
 	@Override
 	public void render(GraphicsContext gfx) {
-		// TODO Auto-generated method stub
 		
+        for(int x = 0; x < Main.WIDTH + spaceWidth; x += spaceWidth) {
+            for(int y = 0; y < Main.HEIGHT * 2; y += spaceHeight) {
+                gfx.drawImage(space, x + xStart, y + yStart);
+            }
+        }
 	}
 
 	@Override
