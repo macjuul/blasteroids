05d672423c8774acd015f9ddd07aa1c654a73c26
Julian <minecraftmacjuul@gmail.com>
2016-06-24 11:00:47 +0200

    Improved Spaceship class

--- src/main/java/net/exodiusmc/blasteroids/entities/Spaceship.java
+++ src/main/java/net/exodiusmc/blasteroids/entities/Spaceship.java
@@ -1,8 +1,11 @@
 package net.exodiusmc.blasteroids.entities;
 
+import net.exodiusmc.blasteroids.enums.SpaceshipType;
+
 public class Spaceship extends Entity {
 	private double angle;
-	private double velox;
-	private double voloy;
-
+	private double vel_x;
+	private double vol_y;
+	private SpaceshipType type;
+	
 }
