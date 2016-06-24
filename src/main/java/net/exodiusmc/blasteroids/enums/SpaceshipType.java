912ef8b12e04592b6009bef84d58a2163c09b009
Julian <minecraftmacjuul@gmail.com>
2016-06-24 10:52:50 +0200

    Added spaceship skin Enum and sprites

---
+++ src/main/java/net/exodiusmc/blasteroids/enums/SpaceshipType.java
@@ -0,0 +1,23 @@
+package net.exodiusmc.blasteroids.enums;
+
+import javafx.scene.image.Image;
+import net.exodiusmc.blasteroids.manager.MediaManager;
+
+public enum SpaceshipType {
+	DEFAULT("default"),
+	BLUE_BIRD("blue_bird"),
+	GREEN_FLAME("green_flame"),
+	EXO_FIGHTER("exo_fighter"),
+	AIR_WING("air_wing"),
+	ARMAGEDDON("armageddon");
+	
+	private Image skin;
+	
+	private SpaceshipType(String file) {
+		this.skin = MediaManager.getManager().getImage(file);
+	}
+	
+	public Image getSkinImage() {
+		return this.skin;
+	}
+}
