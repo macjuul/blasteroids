87887dca06606a744ce07da9de122548adae84c5
Julian <minecraftmacjuul@gmail.com>
2016-06-24 11:11:46 +0200

    Added misc texture & added more powerup types

--- src/main/java/net/exodiusmc/blasteroids/enums/PowerupType.java
+++ src/main/java/net/exodiusmc/blasteroids/enums/PowerupType.java
@@ -1,7 +1,11 @@
 package net.exodiusmc.blasteroids.enums;
 
 public enum PowerupType {
-	HEALTH_PACK,
-	SPEED_BOOST,
-	SUPER_BLASTER;
+	HEALTH_PACK,		// Increases your HP
+	SPEED_BOOST,		// Increases your movement speed
+	SUPER_BLASTER,		// Fires a 3 bullet burst when shot
+	WIDE_SPREAD,		// Shoots 2 extra bullets on each side, going into a slightly altered direction
+	PIERCING_BULLETS,	// Bullets will travel through asteroids
+	SHIELD,				// Reduces incoming damage with 40%
+	ENERGY_BURST;		// Breaks all asteroids currently within sight
 }
