1070e109e7a658805e75e0fd529b0280e0105fb2
Julian <minecraftmacjuul@gmail.com>
2016-06-24 11:37:49 +0200

    Added bullet class

---
+++ src/main/java/net/exodiusmc/blasteroids/entities/Bullet.java
@@ -0,0 +1,10 @@
+package net.exodiusmc.blasteroids.entities;
+
+import javafx.scene.paint.Color;
+
+public class Bullet extends Entity {
+	private int angle;
+	private Spaceship origin;
+	private Color bulletColor;
+	
+}
